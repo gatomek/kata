d8e9a0a81165be3a54661e1d5d1f02073d804d38
Gatomek <tomasz.galinski@gmail.com>
2024-08-21 15:43:25 +0200

    sb-rest-param-validator

---
+++ sb-rest-param-validator/src/main/java/pl/gatomek/katasbrestparamvalidator/KataSbRestParamValidatorApplication.java
@@ -0,0 +1,13 @@
+package pl.gatomek.katasbrestparamvalidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataSbRestParamValidatorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KataSbRestParamValidatorApplication.class, args);
+  }
+
+}
